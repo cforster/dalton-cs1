db52b24cd770417ea63d7ee9cdfb9f04582da539
Charles Forster <charlie@forsters.com>
2013-05-16 17:01:23 -0400

    Break apart sound, remove color

--- DaltonObjects/src/org/dalton/DaltonMidi.java
+++ DaltonObjects/src/org/dalton/DaltonMidi.java
@@ -5,8 +5,6 @@ package org.dalton;
  * @author Charlie Forster
  */
 
-import java.io.IOException;
-
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
@@ -15,11 +13,10 @@ import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
-public class DaltonSound {	
+public class DaltonMidi {	
 	private Sequencer player;
 	private Sequence seq;
 	private Track track;
-	private String voice;
 	
 	private int tempo;
 	private int beatCounter; 	
@@ -27,7 +24,7 @@ public class DaltonSound {
 	/**
 	 * Constructor for the DaltonMidi Class.
 	 */
-	public DaltonSound() {
+	public DaltonMidi() {
 		try {
 			//create midi track:
 			player = MidiSystem.getSequencer();
@@ -50,8 +47,7 @@ public class DaltonSound {
 		{
 			ex.printStackTrace();
 		}
-		
-		voice = "Fred"; //default
+
 	}	
 
 	/**
@@ -82,65 +78,6 @@ public class DaltonSound {
 		}
 
 	}
-
-	/**
-	 * use the mac tts generator to speak
-	 */
-	public void say(String s)
-	{
-		try {
-			Runtime.getRuntime().exec("say -v " + voice + " \"" + s + "\"");
-		} catch (IOException e) {
-			System.err.println("say failed");
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * set voice
-	 * Agnes               en_US    # Isn't it nice to have a computer that will talk to you?<br>
-	 * Albert              en_US    #  I have a frog in my throat. No, I mean a real frog!<br>
-	 * Alex                en_US    # Most people recognize me by my voice.<br>
-	 * Bad News            en_US    # The light you see at the end of the tunnel is the headlamp of a fast approaching train.<br>
-	 * Bahh                en_US    # Do not pull the wool over my eyes.<br>
-	 * Bells               en_US    # Time flies when you are having fun.<br>
-	 * Boing               en_US    # Spring has sprung, fall has fell, winter's here and it's colder than usual.<br>
-	 * Bruce               en_US    # I sure like being inside this fancy computer<br>
-	 * Bubbles             en_US    # Pull the plug! I'm drowning!<br>
-	 * Cellos              en_US    # Doo da doo da dum dee dee doodly doo dum dum dum doo da doo da doo da doo da doo da doo da doo<br>
-	 * Deranged            en_US    # I need to go on a really long vacation.<br>
-	 * Fred                en_US    # I sure like being inside this fancy computer<br>
-	 * Good News           en_US    # Congratulations you just won the sweepstakes and you don't have to pay income tax again.<br>
-	 * Hysterical          en_US    # Please stop tickling me!<br>
-	 * Junior              en_US    # My favorite food is pizza.<br>
-	 * Kathy               en_US    # Isn't it nice to have a computer that will talk to you?<br>
-	 * Pipe Organ          en_US    # We must rejoice in this morbid voice.<br>
-	 * Princess            en_US    # When I grow up I'm going to be a scientist.<br>
-	 * Ralph               en_US    # The sum of the squares of the legs of a right triangle is equal to the square of the hypotenuse.<br>
-	 * Trinoids            en_US    # We cannot communicate with these carbon units.<br>
-	 * Vicki               en_US    # Isn't it nice to have a computer that will talk to you?<br>
-	 * Victoria            en_US    # Isn't it nice to have a computer that will talk to you?<br>
-	 * Whisper             en_US    # Pssssst, hey you, Yeah you, Who do ya think I'm talking to, the mouse?<br>
-	 * Zarvox              en_US    # That looks like a peaceful planet.<br>
-	 */
-	public void setVoice(String voice) {
-		this.voice = voice;
-	}
-	
-	/**
-	 * wait a given number of milliseconds
-	 * @param millis the amount of time to wait
-	 */
-	public void sleep(int millis)
-	{
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
 
 	/**
 	 * push a rest in the song.
